c9fdc6b85368915f2c8ff5e0e48e2673b4583eb0
Dilukshan <dilukshan2020da@gmail.com>
2025-06-16 13:07:59 +0530

    add middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+import admin from '../utils/firebase.js';
+
+const verifyToken = async () => {
+    const token = req.headers.authorization?.split("Bearer ")[1];
+
+    if (!token) {
+        return res.status(401).send("Unauthorized");
+    }
+
+    try {
+        const decodedToken = await admin.auth().verifyIdToken(token);
+        req.user = decodedToken;
+        next();
+    } catch (error) {
+        res.status(403).send("Token is invalid");
+    }
+}
+
+module.exports = verifyToken;
