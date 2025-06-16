a846a5d2a89c4ccb23818b88567f1e2a40c77870
Dilukshan <dilukshan2020da@gmail.com>
2025-06-16 12:37:32 +0530

    create api for post signup

--- server.js
+++ server.js
@@ -14,6 +14,39 @@ app.get('/', (req, res) => {
     res.send(`Server is running on http://localhost:${PORT}`);
 })
 
+app.post('/api/signup', async (req, res) => {
+    try {
+        const { uid, firstName, lastName, email, role } = req.body;
+
+        const checkUserExists = await User.findOne( { uid } );
+        if (!checkUserExists) {
+            const newUser = new User({
+                uid,
+                firstName,
+                lastName,
+                email,
+                role
+            })
+            await newUser.save();
+            return res.status(201).json({
+                message: "User created successfully",
+                role: newUser.role,
+            })
+        } else {
+            return res.status(400).json({
+                message: "User already exists",
+                role: checkUserExists.role,
+            })
+        }
+    } catch (error) {
+        console.error("Error creating user:", error);
+        return res.status(500).json({
+            message: "Internal server error",
+            error: error.message
+        });
+    }
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 })
