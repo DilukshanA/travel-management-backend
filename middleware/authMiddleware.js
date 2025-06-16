b6bf0f15c12deee75667e29bd4eb87bbd6335ef3
Dilukshan <dilukshan2020da@gmail.com>
2025-06-16 20:21:43 +0530

    update middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,18 +1,25 @@
 import admin from '../utils/firebase.js';
 
 const verifyToken = async (req, res, next) => {
-    const token = req.headers.authorization?.split("Bearer ")[1];
 
-    if (!token) {
-        return res.status(401).send("Unauthorized");
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+
+        return res.status(401).json({ 
+            message: 'Unauthorized: No token provided'
+        });
     }
+
+    const token = authHeader.split('Bearer ')[1];
 
     try {
         const decodedToken = await admin.auth().verifyIdToken(token);
         req.user = decodedToken;
         next();
     } catch (error) {
-        res.status(403).send("Token is invalid");
+        console.error('Token verification failed:', error);
+        res.status(403).json({ message: 'Forbidden: Invalid token' });
     }
 }
 
