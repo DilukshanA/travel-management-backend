c16bfeef0f14bf17ef0062195dadb41a8027076e
Dilukshan <dilukshan2020da@gmail.com>
2025-06-17 11:47:33 +0530

    clean structure

---
+++ src/api/controllers/authController.js
@@ -0,0 +1,34 @@
+import { User } from '../../models/user.js';
+
+export const signupWithEmailPassword = async (req, res) => {
+    try {
+        const { uid, firstName, lastName, email, role } = req.body;
+
+        const checkUserExists = await User.findOne( { uid } );
+        if (!checkUserExists) {
+            const newUser = new User({
+                uid,
+                firstName,
+                lastName,
+                email,
+                role
+            })
+            await newUser.save();
+            return res.status(201).json({
+                message: "User registered successfully!",
+                role: newUser.role,
+            })
+        } else {
+            return res.status(400).json({
+                message: "User already exists",
+                role: checkUserExists.role,
+            })
+        }
+    } catch (error) {
+        console.error("Error creating user:", error);
+        return res.status(500).json({
+            message: "Internal server error",
+            error: error.message
+        });
+    }
+}
--- src/server.js
+++ src/server.js
@@ -1,9 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
-import { User } from './models/user.js';
-import verifyToken from './api/middleware/authMiddleware.js';
 import cors from 'cors';
+import authRouter from './api/routes/authRoutes.js';
 
 dotenv.config();
 
@@ -21,38 +20,7 @@ app.get('/', (req, res) => {
     res.send(`Server is running on http://localhost:${PORT}`);
 })
 
-app.post('/api/auth/signup', verifyToken, async (req, res) => {
-    try {
-        const { uid, firstName, lastName, email, role } = req.body;
-
-        const checkUserExists = await User.findOne( { uid } );
-        if (!checkUserExists) {
-            const newUser = new User({
-                uid,
-                firstName,
-                lastName,
-                email,
-                role
-            })
-            await newUser.save();
-            return res.status(201).json({
-                message: "User registered successfully!",
-                role: newUser.role,
-            })
-        } else {
-            return res.status(400).json({
-                message: "User already exists",
-                role: checkUserExists.role,
-            })
-        }
-    } catch (error) {
-        console.error("Error creating user:", error);
-        return res.status(500).json({
-            message: "Internal server error",
-            error: error.message
-        });
-    }
-});
+app.use('/api/auth', authRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
