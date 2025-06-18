e6684ebb156e50937acb24c5952541b72fe2654a
Dilukshan <dilukshan2020da@gmail.com>
2025-06-18 11:01:25 +0530

    add new middlewares to sign up process

--- src/api/routes/authRoutes.js
+++ src/api/routes/authRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import verifyToken from '../middleware/authMiddleware.js';
-import { signupWithEmailPassword } from '../controllers/authController.js';
+//import { signupWithEmailPassword } from '../controllers/authController.js';
+import verifyToken from '../middleware/verifyAuthToken.js';
+import { signupWithEmailPassword } from '../middleware/signupWithEmailPassword.js';
+import otpMiddleware from '../middleware/handleOtp.js';
+import { sendOTPMail } from '../controllers/sendMailController.js';
 
 const authRouter = express.Router();
 
-authRouter.post('/signup', verifyToken, signupWithEmailPassword);
+authRouter.post('/signup', verifyToken, signupWithEmailPassword, otpMiddleware, sendOTPMail);
 
 export default authRouter;
