543eb2d808a0b04aaaeb654a271b69be643cd9ca
Dilukshan <dilukshan2020da@gmail.com>
2025-06-27 22:14:52 +0530

    change file names and controller

--- src/api/routes/authRoutes.js
+++ src/api/routes/authRoutes.js
@@ -4,7 +4,7 @@ import express from 'express';
 // import { signupWithEmailPassword } from '../middleware/signupWithEmailPassword.js';
 import otpMiddleware from '../middleware/handleOtp.js';
 import { sendOTPMail } from '../controllers/sendMailController.js';
-import userVerifySignup from '../controllers/userVerifySignup.js';
+import userOtpVerifySignup from '../controllers/otpController.js';
 import signupFirebaseAndMongoDb from '../middleware/signupFirebaseAndMongoDb.js';
 import signUpWithGoogle from '../controllers/signUpWithGoogle.js';
 import authenticateJwt from '../middleware/auth/authenticateJwt.js';
@@ -14,7 +14,7 @@ const authRouter = express.Router();
 
 // authRouter.post('/signup', verifyToken, signupWithEmailPassword, otpMiddleware, sendOTPMail);
 authRouter.post('/signup', signupFirebaseAndMongoDb, otpMiddleware, sendOTPMail);
-authRouter.post('/signup-otp-verify', userVerifySignup);
+authRouter.post('/signup-otp-verify', userOtpVerifySignup);
 authRouter.post('/signup-with-google', signUpWithGoogle);
 // test route to check JWT authentication
 authRouter.get('/test/protected', authenticateJwt, protectedTest);
