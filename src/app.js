ac0522f1727906310037e6e1730a6723933072c1
Dilukshan <dilukshan2020da@gmail.com>
2025-06-27 21:38:02 +0530

    create new route -user

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import authRouter from './api/routes/authRoutes.js';
 import sendMailRouter from './api/routes/sendMailRoutes.js';
 import cookieParser from 'cookie-parser';
+import userRouter from './api/routes/userRoutes.js';
 
 const app = express();
 
@@ -18,5 +19,6 @@ app.get('/', (req, res) => {
 
 app.use('/api/auth', authRouter);
 app.use('/api/sendMail', sendMailRouter);
+app.use('/api/', userRouter);
 
 export default app;
