36e61a33080174050f664968f73ffc664d4e4534
Dilukshan <dilukshan2020da@gmail.com>
2025-06-28 21:01:59 +0530

    fix and create new api for user

--- src/api/controllers/userController.js
+++ src/api/controllers/userController.js
@@ -1,6 +1,24 @@
+import { User } from "../../models/user.js";
 
-const getUserById = async (req, res) => {
-    const userId = req.body
+const getUserData = async (req, res) => {
+    try {
+        const uid = req.user.uid; // get user from authenticateJWT middleware
+        const user = await User.findOne({ uid });
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+        return res.status(200).json({
+            message: "User retrieved successfully",
+            user
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: "Internal server error",
+            error: error.message
+        });
+    }
 }
 
-export default getUserById;
+export { getUserData };
