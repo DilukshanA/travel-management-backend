14ed1cd50714506b25236e6dbbbd350ad1601e99
Dilukshan <dilukshan2020da@gmail.com>
2025-06-28 13:29:24 +0530

    create alreadyverified middleware to prevent re again resend and verify

--- src/api/routes/authRoutes.js
+++ src/api/routes/authRoutes.js
@@ -9,13 +9,14 @@ import signupFirebaseAndMongoDb from '../middleware/signupFirebaseAndMongoDb.js'
 import signUpWithGoogle from '../controllers/signUpWithGoogle.js';
 import authenticateJwt from '../middleware/auth/authenticateJwt.js';
 import { protectedTest } from '../controllers/test/protected.js';
+import checkUserAlreadyVerified from '../middleware/userVerfyMiddleware.js';
 
 const authRouter = express.Router();
 
 // authRouter.post('/signup', verifyToken, signupWithEmailPassword, otpMiddleware, sendOTPMail);
 authRouter.post('/signup', signupFirebaseAndMongoDb, otpGenerateAndStoreDb, sendOTPMail);
-authRouter.post('/signup-otp-verify', userOtpVerifySignup);
-authRouter.post('/signup-resend-otp', otpGenerateAndStoreDb, sendOTPMail);
+authRouter.post('/signup-otp-verify', checkUserAlreadyVerified, userOtpVerifySignup);
+authRouter.post('/signup-resend-otp', checkUserAlreadyVerified, otpGenerateAndStoreDb, sendOTPMail);
 authRouter.post('/signup-with-google', signUpWithGoogle);
 // test route to check JWT authentication
 authRouter.get('/test/protected', authenticateJwt, protectedTest);
