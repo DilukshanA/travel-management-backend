02cbe9b8245c71d23b7d32f7499cd14c63bd0861
Dilukshan <dilukshan2020da@gmail.com>
2025-06-29 17:24:39 +0530

    add cookie for continue with google

--- src/api/controllers/signUpWithGoogle.js
+++ src/api/controllers/signUpWithGoogle.js
@@ -1,5 +1,6 @@
 import admin from "../../config/firebase.js";
 import { User } from "../../models/user.js";
+import { generateJwtToken } from "../../utils/jwt.js";
 
 const signUpWithGoogle = async (req, res) => {
 
@@ -22,7 +23,7 @@ const signUpWithGoogle = async (req, res) => {
         const user = await User.findOne({ uid, email });
 
         // if not user exists, create a new user
-        //this will be used to create a new user in MongoDB
+        // this will be used to create a new user in MongoDB
         // for new user or lazy sync with mongoDB when server error occurs
         if (!user) {
             const newUser = new User({
@@ -34,12 +35,40 @@ const signUpWithGoogle = async (req, res) => {
                 role: "Passenger" // default role, can be changed later
             })
             await newUser.save();
+
+            // create JWT token
+            const token = generateJwtToken({
+                uid: newUser.uid,
+                email: newUser.email,
+            })
+
+            // set cookie with JWT token
+            res.cookie("auth_token", token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+                maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+            })
+
             return res.status(200).json({
                 message: "User registered successfully!",
                 role: newUser.role
             })
         } else {
             // if user exists, sign in the user
+            // create JWT token
+            const token = generateJwtToken({
+                uid: newUser.uid,
+                email: newUser.email,
+            })
+
+            // set cookie with JWT token
+            res.cookie("auth_token", token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+                maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+            })
             return res.status(201).json({
                 message: "User sign in successfully!",
                 role: user.role
