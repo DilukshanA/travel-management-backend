759bc04c2b641aa6acf739af7a1989f86124b255
Dilukshan <dilukshan2020da@gmail.com>
2025-06-29 17:07:56 +0530

    create update controller

--- src/api/routes/userRoutes.js
+++ src/api/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getUserData } from '../controllers/userController.js';
+import { getUserData, updateUserData } from '../controllers/userController.js';
 import authenticateJwt from '../middleware/auth/authenticateJwt.js';
 
 const userRouter = express.Router();
 
 userRouter.get('/get-user-data', authenticateJwt, getUserData);
+userRouter.put('/update-user-data', authenticateJwt, updateUserData);
 
 export default userRouter;
