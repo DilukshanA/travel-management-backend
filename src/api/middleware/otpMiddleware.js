06abe3d18df327f239a4b80939b6e6aa1696a9d7
Dilukshan <dilukshan2020da@gmail.com>
2025-07-01 14:01:11 +0530

    removed and update

--- src/api/middleware/otpMiddleware.js
+++ src/api/middleware/otpMiddleware.js
@@ -1,8 +1,9 @@
 import { Otp } from "../../models/otp.js";
+import { User } from "../../models/user.js";
 import generateOTP from "../../utils/otp.js";
 
 // this will call generteOtp and store the otp in the database and work as middleware
-const otpGenerateAndStoreDb = async (req, res, next) => {
+export const otpGenerateAndStoreDb = async (req, res, next) => {
 
     const { email } = req.body;
     const otp = generateOTP();
@@ -42,4 +43,29 @@ const otpGenerateAndStoreDb = async (req, res, next) => {
     }
 }
 
-export default otpGenerateAndStoreDb;
+// this middleware checks if the user is already verified - to prevent re-verification
+export const checkUserAlreadyVerified = async (req, res, next) => {
+    const email = req.body.email;
+    try {
+        const user = await User.findOne({ email });
+        if(!user) {
+            return res.status(404).json({
+                message: "User not found. Please sign up first."
+            })
+        }
+        if(user.verified) {
+            return res.status(403).json({
+                message : "User already verified. Please login."
+            })
+        }
+        if(!user.verified) {
+            next(); // proceed to the next middleware or controller
+        }
+    } catch (error) {
+        return res.status(500).json({
+            message: "Internal server error",
+            error: error.message
+        })
+    }
+
+}
--- src/api/routes/sendMailRoutes.js
+++ src/api/routes/sendMailRoutes.js
@@ -1,10 +1,8 @@
 import express from 'express';
-import { sendOTPMail, sendTestMail } from '../controllers/sendMailController.js';
-import otpMiddleware from '../middleware/otpMiddleware.js';
+import { sendTestMail } from '../controllers/sendMailController.js';
 
 const sendMailRouter = express.Router();
 
 sendMailRouter.post('/test', sendTestMail); // this is for testing purposes only
-sendMailRouter.post('/signup-otp', otpMiddleware, sendOTPMail); // this is not used now
 
 export default sendMailRouter;
